e2800d8b6f8a9a47a4e48b248e95090c8f3e8a6c
unknown <albertramadhanvanwijk@gmail.com>
2022-06-16 00:23:58 +0700

    Deploy to github page

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var cards = document.querySelectorAll('.product-box');
+
+[...cards].forEach((card)=> {
+    card.addEventListener('mouseover', function(){
+        card.classList.add('is-hover');
+    })
+    card.addEventListener('mouseover', function(){
+        card.classList.remove('is-hover');
+    })
+})
